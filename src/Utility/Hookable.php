2cb4ad688d2fe142cc9d31d44dd8c1d60b3ffc83
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-10-17 18:43:39 -0500

    Add Hookable utility.

---
+++ src/Utility/Hookable.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Interface for objects that need to register hooks with WordPress.
+ *
+ * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package WDS\OopsWP\Utility
+ */
+
+namespace WDS\OopsWP\Utility;
+
+/**
+ * Interface Hookable
+ *
+ * @package WDS\OopsWP\Utility
+ */
+interface Hookable {
+	/**
+	 * Register actions and filters with WordPress.
+	 */
+	public function register_hooks();
+}
