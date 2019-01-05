9b90bb47c203ab0ae10eab1b54390e0d66e97866
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-01-05 12:05:53 -0600

    Add Editorblock abstract class to project.

---
+++ src/Structure/Editor/EditorBlock.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Defines a contract for registering new editor blocks.
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package WebDevStudios\OopsWP\Structure\Editor
+ * @since   2019-01-04
+ */
+
+namespace WebDevStudios\OopsWP\Structure\Editor;
+
+use WebDevStudios\OopsWP\Utility\Registerable;
+use WebDevStudios\OopsWP\Utility\RootPathDependent;
+
+/**
+ * Class EditorBlock
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package WebDevStudios\OopsWP\Structure\Editor
+ * @since   2019-01-04
+ */
+abstract class EditorBlock implements Registerable {
+	use RootPathDependent;
+
+	/**
+	 * Register the block with WordPress.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-01-04
+	 * @return void
+	 */
+	public function register() {
+		$this->register_script();
+		$this->register_style();
+		$this->register_type();
+	}
+
+	/**
+	 * Register the JavaScript assets that power the block.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-01-04
+	 */
+	abstract public function register_script();
+
+	/**
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-01-05
+	 * @return mixed
+	 */
+	abstract public function register_style();
+
+	/**
+	 * Register the block type information.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-01-04
+	 */
+	abstract public function register_type();
+}
