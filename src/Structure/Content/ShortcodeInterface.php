966f271e516aef742b401a8e0cae4fe3d7fbb2fe
Jeremy Ward <jeremy@jmichaelward.com>
2020-01-31 13:17:08 -0600

    Add the new process_output method to the ShortcodeInterface interface.
    The PHPDoc in this method links to the example abstract Shortcode to help
    engineers understand how to use this interface should they decide that they want
    to create a standalone abstract class of their own.

--- src/Structure/Content/ShortcodeInterface.php
+++ src/Structure/Content/ShortcodeInterface.php
@@ -20,5 +20,19 @@ use WebDevStudios\OopsWP\Utility\Renderable;
  * @since   2019-04-01
  */
 interface ShortcodeInterface extends Registerable, Renderable {
-
+	/**
+	 * Intermediary callback for a shortcode object.
+	 *
+	 * Implementing classes should consider extending this method to perform any necessary validation of
+	 * attributes and content from the database, before finally calling `render` from within this method.
+	 *
+	 * @see Shortcode::process_output() for example default implementation.
+	 *
+	 * @param array  $attributes The shortcode attributes.
+	 * @param string $content    The shortcode content.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-01-31
+	 */
+	public function process_output( array $attributes = [], string $content = '' );
 }
